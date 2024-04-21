86e130f8417257c15d18f980984b81ff8a077c64
Muhammad Fahmi <96282872+fahmihilmi@users.noreply.github.com>
2024-04-21 21:14:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
-print ('Hello World!')
+# app.py
+import streamlit as st
+
+def main():
+    st.title("Simple Streamlit App")
+    
+    # Add a text input widget
+    user_input = st.text_input("Enter your name", "John Doe")
+    
+    # Display the input value
+    st.write("Hello,", user_input)
+
+if __name__ == "__main__":
+    main()
